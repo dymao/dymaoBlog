4aa61ad07ae0aa72e2594bfe8ef1b0d3b593f6d9
Mervin <dymaosuper@163.com>
2017-11-12 11:27:48 +0800

    Initial commit

---
+++ src/main/java/com/dymao/DymaoBlogApplication.java
@@ -0,0 +1,12 @@
+package com.dymao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DymaoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DymaoBlogApplication.class, args);
+	}
+}
